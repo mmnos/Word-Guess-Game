b11b05aa09f21264c39863c54e868bd6fdc7ff6f
Michael Nos <mnos9897@gmail.com>
2019-09-19 13:37:20 -0400

    add wins after checking answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,20 +17,64 @@ let correctGuess;
 
 $(document).ready(function() {
 
-  console.log(randWord);
-
   let getWord = () => {
 
+    $("#randWord").empty();
+    answer = [];
+
+    randWord = words[Math.floor(Math.random() * words.length)];
+    console.log(randWord);
     for ( i = 0; i < randWord.length; i++ ) {
 
       answer[i] = "_";
 
-      $(".randWord").html(answer.join(" "));
+      $("#randWord").html(answer.join(" "));
 
     }
 
   };
 
+  $("#emptyBtn").on("click", (e) => {
+    $("#randWord").empty();
+  })
+
+  let displayStats = () => {
+
+    $("p.wins").html(`Wins : ${wins}`);
+    $("p.losses").html(`Losses : ${losses}`);
+    $("p.guesses").html(`Guesses left : ${guessesLeft}`);
+
+  }
+
+  let checkAnswer = () => {
+
+    if (!answer.includes("_")) {
+
+      resetGame(true);
+
+    }
+
+    if ( guessesLeft === 0 ) {
+
+      resetGame(false);
+
+    }
+
+  };
+
+  let resetGame = (bool) => {
+
+    if (bool) {
+      wins++
+    } else losses++
+
+    guessesLeft = 10;
+    displayStats();
+    getWord();
+
+  };
+
+  displayStats();
   getWord();
 
   $(document).on('keyup', (e) => {
@@ -39,7 +83,7 @@ $(document).ready(function() {
       return;
     }
 
-    guessesLeft -= 1;
+    guessesLeft--;
 
     userGuess = e.key.toLowerCase();
 
@@ -47,23 +91,27 @@ $(document).ready(function() {
 
     if ( correctGuess > -1 ) {
 
-      for ( j = 0; j < answer.length; j++ ) {
+      // for ( j = 0; j < answer.length; j++ ) {
 
-        if ( randWord[j] === userGuess ) {
+      //   if ( randWord[j] === userGuess ) {
 
-          answer[j] = userGuess;
+      //     answer[j] = userGuess;
 
-        }
+      //   }
 
-      }
+      // }
 
-      $(".randWord").html(answer.join(" "));
+      answer.forEach((letter, index) => {
+        randWord[index] === userGuess && (answer[index] = userGuess);
+      });
 
-    } else {
-
-
+      $("p#randWord").html(answer.join(" "));
 
     }
+
+    $("p.guesses").html(`Guesses left : ${guessesLeft}`);
+
+    checkAnswer();
 
   });
 
